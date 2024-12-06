31f36521b7abe6ef12a9afc330bb069c28c42765
Meterius <Meterius@users.noreply.github.com>
2024-12-06 14:42:31 +0100

    solved day 5 part 2 very inefficiently

--- src/day_5.rs
+++ src/day_5.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::str::FromStr;
@@ -28,7 +28,7 @@ pub fn part_1(data: File) -> usize {
 
         let med = xs[xs.len() / 2];
 
-        if xs.into_iter().tuple_windows().all(|(x, y)| !forbidden.contains(&(x, y))) {
+        if (0..xs.len()).all(|i| (i+1..xs.len()).all(|j| !forbidden.contains(&(xs[i], xs[j])))) {
             total += med as usize;
         }
     }
@@ -37,5 +37,46 @@ pub fn part_1(data: File) -> usize {
 }
 
 pub fn part_2(data: File) -> usize {
-    return 0;
+    let mut total = 0;
+
+    let mut lines = BufReader::new(data).lines().flatten();
+
+    let mut forbidden = HashSet::new();
+
+    while let Some(line) = lines.next() {
+        if line.is_empty() { break; }
+        let (x, y) = line.as_str()
+            .split("|").map(|val| u32::from_str(val).unwrap())
+            .next_tuple().unwrap();
+
+        forbidden.insert((y, x));
+    }
+
+    for line in lines {
+        let mut xs = line
+            .split(",")
+            .map(|val| u32::from_str(val).unwrap())
+            .collect_vec();
+        
+        let mut valid = true;
+        loop {
+            for i in 0..xs.len() {
+                for j in i + 1..xs.len() {
+                    if forbidden.contains(&(xs[i], xs[j])) { 
+                        xs.swap(i, j);
+                        valid = false;
+                        continue;
+                    }
+                }
+            }
+
+            break;
+        }
+
+        if !valid {
+            total += xs[xs.len() / 2] as usize;
+        }
+    }
+
+    return total;
 }
